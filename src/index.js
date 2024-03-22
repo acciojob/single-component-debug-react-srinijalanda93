f7ad2e6de10b9575e7c549cced051a5482d440bf
srinija landa <144719297+srinijalanda93@users.noreply.github.com>
2024-03-22 09:25:04 +0000

    3rd

--- src/index.js
+++ src/index.js
@@ -2,8 +2,5 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 
-const root=document.getElementById("root");
-ReactDOM.render(App(),root)
-//const h1=React.createElement("h1",{id:"test",className:"Abc",style:{color:"red"}},"HelloWorld");
-// ReactDOM.render(h1,divObj);
-// console.log(h1);
+const root = document.getElementById("root");
+ReactDOM.render(<App />, root);
