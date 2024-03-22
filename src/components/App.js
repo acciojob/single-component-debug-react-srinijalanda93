31042e63c2557b34f1e2e66a8ab7bb94c4c31aa8
srinija landa <144719297+srinijalanda93@users.noreply.github.com>
2024-03-22 09:42:32 +0000

    f

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,19 @@
 {/* <p>Now I can render any React component on any DOM node I want using ReactDOM.render</p> */}
 
-export default function Counter(){
-    return(<>
-    <div id="main">
-    <p>Now I can render any React component on any DOM node I want using ReactDOM.render</p> 
-    </div>
-    </>)
-};
+// export default function Counter(){
+//     return(
+//     <div id="main">
+//     <p>Now I can render any React component on any DOM node I want using ReactDOM.render</p> 
+//     </div>
+//     )
+// };
 
+import React from 'react';
+const App = () => {
+  return (
+    <div id="main">
+      <p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+    </div>
+  );
+};
+export default App;
