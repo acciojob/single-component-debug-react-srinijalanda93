5e89a4952fc0d55c694cfa671285271b02603f7c
srinija landa <144719297+srinijalanda93@users.noreply.github.com>
2024-03-22 09:17:34 +0000

    fris

--- src/index.js
+++ src/index.js
@@ -2,5 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 
-
-
+const root=document.getElementById("root");
+ReactDOM.render(App(),root)
+//const h1=React.createElement("h1",{id:"test",className:"Abc",style:{color:"red"}},"HelloWorld");
+// ReactDOM.render(h1,divObj);
+// console.log(h1);
